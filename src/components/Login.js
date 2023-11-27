72f2986239e88a6b31eb181c6bb4d05404a2768c
Samson <samsonsurin22@gmail.com>
2023-11-27 18:46:31 +0530

    changed the position of the post

--- src/components/Login.js
+++ src/components/Login.js
@@ -22,7 +22,7 @@ const Login = (props) => {
             return "Please wait loading"
           },
           icon: true,
-          position: "top-center",
+          position: "bottom-center",
           theme: "colored",
         },
       }
